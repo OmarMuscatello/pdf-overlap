bf21551bd5b43eff5624c66b0cbe228ff3a243e2
Omar Muscatello <7016897+OmarMuscatello@users.noreply.github.com>
2018-01-16 09:42:41 +0100

    Added project files

---
+++ README.md
@@ -0,0 +1,14 @@
+﻿# pdf-overlap
+Overlaps each page of a PDF file to each page of another PDF file using [iTextSharp](https://sourceforge.net/projects/itextsharp/).
+
+## Installation
+- Clone or download this repository
+- Set values of `baseFilePath`, `overFilePath` and `outputFilePath`.
+- Run!
+
+## Constants explaination
+| Constant name    | Description                                                                                                                        |
+|------------------|------------------------------------------------------------------------------------------------------------------------------------|
+| `baseFilePath`   | Path of the file to be overlapped. Each page of this file will be overlapped by the corrisponding page of `overFilePath` PDF file. |
+| `overFilePath`   | Path of the file to overlap. Each page of this file will overlap the corrisponding page of `baseFilePath` PDF file.                |
+| `outputFilePath` | The output file path: `overFilePath` pages over `baseFilePath` pages.                                                              |
---
+++ src/Program.cs
@@ -0,0 +1,109 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.IO;
+
+namespace pdf_overlap
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string baseFilePath = @"<File to be overlapped>";
+            const string overFilePath = @"<File to overlap>";
+            const string outputFilePath = @"<Output file>";
+
+            var result = Overlap(baseFilePath, overFilePath, outputFilePath);
+
+            if (result.HasError)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("DONE!");
+            }
+
+            Console.ReadKey();
+        }
+
+        private class Result
+        {
+            public string Error { get; set; }
+            public bool HasError { get => !string.IsNullOrEmpty(Error); }
+        }
+
+        /// <summary>
+        /// Overlaps each page of <paramref name="overFilePath"/> on the respective page of <paramref name="baseFilePath"/>. 
+        /// </summary>
+        /// <param name="baseFilePath">File to be overlapped</param>
+        /// <param name="overFilePath">File to overlap</param>
+        /// <param name="outputFilePath">Output file</param>
+        /// <returns></returns>
+        private static Result Overlap(string baseFilePath, string overFilePath, string outputFilePath)
+        {
+            var result = new Result();
+
+            PdfReader overReader = null;
+            PdfReader baseReader = null;
+            PdfStamper stamper = null;
+
+            try
+            {
+                overReader = new PdfReader(overFilePath);
+                baseReader = new PdfReader(baseFilePath);
+
+                stamper = new PdfStamper(overReader, new FileStream(outputFilePath, FileMode.Create));
+
+                var numberOfPages = Math.Min(baseReader.NumberOfPages, overReader.NumberOfPages);
+
+                PdfContentByte background;
+
+                for (var i = 1; i <= numberOfPages; i++)
+                {
+                    var page = stamper.GetImportedPage(baseReader, i);
+
+                    var pageWidth = overReader.GetPageSizeWithRotation(i).Width;
+                    var pageHeight = overReader.GetPageSizeWithRotation(i).Height;
+
+                    background = stamper.GetUnderContent(i);
+
+                    switch (page.Rotation)
+                    {
+                        case 0:
+                            background.AddTemplate(page, 1f, 0, 0, 1f, 0, 0);
+                            break;
+                        case 90:
+                            background.AddTemplate(page, 0, -1f, 1f, 0, 0, pageHeight);
+                            break;
+                        case 180:
+                            background.AddTemplate(page, -1f, 0, 0, -1f, pageWidth, pageHeight);
+                            break;
+                        case 270:
+                            background.AddTemplate(page, 0, 1f, -1f, 0, pageWidth, 0);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Error = "Error: " + ex.Message;
+            }
+            finally
+            {
+                if (stamper != null) stamper.Close();
+                if (overReader != null) overReader.Dispose();
+                if (baseReader != null) baseReader.Dispose();
+            }
+
+            if (!File.Exists(outputFilePath))
+            {
+                result.Error = "Output file not found.";
+            }
+
+            return result;
+        }
+
+    }
+}
